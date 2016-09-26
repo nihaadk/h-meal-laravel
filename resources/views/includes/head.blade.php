da92916b79b7961f3cb35a3145e5654ae255cd41
nihadkeric <nihadkeric@gmail.com>
2016-09-26 13:02:19 +0200

    Update chartjs 26.09.2016

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,6 +6,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="/css/main.css" >
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <script src="{{ asset('js/Chart.min.js') }}"></script>
-
+    <<script src="../../../public/js/Chart.min.js" type="text/javascript" charset="utf-8" async defer></script>
+    {{-- <script src="{{ asset('js/Chart.min.js') }}"></script>
+ --}}
 
